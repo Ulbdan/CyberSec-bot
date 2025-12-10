c7ad9bbe0f55985aae52661e9050ca143e6100cc
Ulbdan <52607369+Ulbdan@users.noreply.github.com>
2025-12-10 13:06:45 +0000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,12 +1,13 @@
 class Config:
     # Slack
-    slack_signing_secret = "6e0827e5ea6673ea57b940659c94977e"
-    slack_bot_token = "xoxb-9853101221189-9856580309186-T8axSfJSWZbek6VtdwXOf1ov"
+    slack_signing_secret = "signing_secret_from_slack"
+    slack_bot_token = "xoxb-token-from-slack"
 
     # HuggingFace
-    hf_token = "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"
+    hf_token = "hf_token"
     hf_model = "google/gemma-2-2b-it"
 
     # ngrok
-    ngrok_auth_token = "356NuOVxvVmym4iYyQUCd5QAVvf_7KWPgvFXfcYqgbXaPSjNT"
+    ngrok_auth_token = "ngrok-token"
     port = 8000
+
